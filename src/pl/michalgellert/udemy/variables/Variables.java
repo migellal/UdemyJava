004cbf655bf18cdef63606db99fef6e59515c11a
migellal <michalgellert@gmail.com>
2018-01-07 00:03:25 +0100

    variables shortcuts

--- src/pl/michalgellert/udemy/variables/Variables.java
+++ src/pl/michalgellert/udemy/variables/Variables.java
@@ -36,20 +36,43 @@ public class Variables {
         System.out.println("Sum: " + b + b1);
         System.out.println("Sum: " + (b + b1));
 
-        int x = 1+2;
-        int x1 = x+b;
-        int y = b-b1;
-        int xx = y*y;
-        int yy = x1/x;
-        double dd = x1/x;
+        int x = 1 + 2;
+        int x1 = x + b;
+        int y = b - b1;
+        int xx = y * y;
+        int yy = x1 / x;
+        double dd = x1 / x;
         double dx = 3;
         double dy = 7;
-        double dz = dy/dx;
-        int r = x1%x;
+        double dz = dy / dx;
+        int r = x1 % x;
         System.out.println(x1);
         System.out.println(yy); // 130/3 = 43 i 1/3
         System.out.println(dd); // 130/3 = 43 i 1/3
         System.out.println(dz); // 7/3 = 43 i 1/3
         System.out.println(r); // 130/3 = 43 i 1/3
+
+        // -----------------------------//
+
+        int i1 = 10;
+        int i2 = 20;
+        int i3 = 30;
+        int i4 = 40;
+        System.out.println("------------------------------------------------");
+        System.out.println(i1++); //i1 = i1+1;
+        System.out.println(i1); //i1 = i1+1;
+        System.out.println(++i2); //i2 = i2+1;
+        System.out.println(i3--); //i3 = i3-1;
+        System.out.println(i3); //i3 = i3-1;
+        System.out.println(--i4); //i4 = i4-1;
+
+        x += 5; // x = x + 5;
+        x -= 5; // x = x - 5;
+        x *= 5; // x = x * 5;
+        x /= 5; // x = x / 5;
+        x %= 5; // x = x % 5;
+
+        System.out.println(x);
+
     }
 }
