27acb9c780f4485a94c0095e1ada528f43c56bc1
migellal <michalgellert@gmail.com>
2018-01-06 22:48:09 +0100

    args

--- src/pl/michalgellert/udemy/firstprogram/Main.java
+++ src/pl/michalgellert/udemy/firstprogram/Main.java
@@ -3,7 +3,7 @@ package pl.michalgellert.udemy.firstprogram;
 // to jest tekst, który nic nie zmieni
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Intellij!");
+        System.out.println("Hello, Michał");
         /*
 to jest komentarz na kilka linii
 np na dwie
