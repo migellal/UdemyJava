f6b76f720408b064015b9cf19398af41998f5426
migellal <michalgellert@gmail.com>
2018-01-07 01:30:47 +0100

    foreach

--- src/pl/michalgellert/udemy/loops/Loops.java
+++ src/pl/michalgellert/udemy/loops/Loops.java
@@ -4,7 +4,7 @@ public class Loops {
 
     public static void main(String[] args) {
 
-        for(int i = 0; i<30; i++) { // for(int i = 1; i<=30; i++)
+        for (int i = 0; i < 30; i++) { // for(int i = 1; i<=30; i++)
             System.out.println("nr# " + i);
         }
 
@@ -13,16 +13,32 @@ public class Loops {
 //        }
 
         int l = 20;
-        for(; l>10; l--) {
+        for (; l > 10; l--) {
             System.out.println(l);
         }
 
-        for(int i = 0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.print(i + " - ");
-            for (int j = 0; j<10; j++) {
+            for (int j = 0; j < 10; j++) {
                 System.out.print(j + ", ");
             }
             System.out.println("");
         }
+
+        int[] ints = {4, 3, 65, 123, 2, 90, 15};
+
+        for(int i = 0; i<ints.length; i++) {
+            System.out.println(ints[i]);
+        }
+
+        // ----- foreach ---------
+
+        for(int i : ints) {
+            System.out.println(i);
+        }
+
+        // ---------while--------
+
+
     }
 }
