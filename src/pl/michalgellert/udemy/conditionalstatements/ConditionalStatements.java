935a14f98435a778713f62f7e9f8cfbd39abc49e
migellal <michalgellert@gmail.com>
2018-01-09 00:37:45 +0100

    switch case

--- src/pl/michalgellert/udemy/conditionalstatements/ConditionalStatements.java
+++ src/pl/michalgellert/udemy/conditionalstatements/ConditionalStatements.java
@@ -23,5 +23,29 @@ public class ConditionalStatements {
                 System.out.println("a");
             }
         }
+
+        for (int i : ints) {
+            if (i < 3) {
+                System.out.println("i<3" + i);
+            } else {
+                System.out.println("i>3" + i);
+            }
+        }
+        // switch case //
+
+        int menu = 4;
+        switch (menu) {
+            case 1:
+                System.out.println("main menu");
+                break;
+            case 2:
+                System.out.println("game menu");
+                break;
+            case 3:
+                System.out.println("end menu");
+                break;
+            default:
+                System.out.println("invalid parameter");
+        }
     }
 }
