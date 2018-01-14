721164464c74a493d453cb9e0f392cae3ec734f0
migellal <michalgellert@gmail.com>
2018-01-14 20:54:47 +0100

    list

---
+++ src/pl/michalgellert/udemy/collections/Collections.java
@@ -0,0 +1,14 @@
+package pl.michalgellert.udemy.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("tekst");
+        list.add("tekst");
+        System.out.println(list);
+    }
+}
