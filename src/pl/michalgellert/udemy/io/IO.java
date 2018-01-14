bce8bd1ef981a582f97eac736cea0445e294e1cd
migellal <michalgellert@gmail.com>
2018-01-15 00:01:56 +0100

    save to file

--- src/pl/michalgellert/udemy/io/IO.java
+++ src/pl/michalgellert/udemy/io/IO.java
@@ -1,13 +1,10 @@
 package pl.michalgellert.udemy.io;
 
-import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -27,14 +24,24 @@ public class IO {
 //        }
 
         String separator = File.separator;
-        File file = new File("C:"+separator+"dev\\repos\\java\\src\\pl\\michalgellert\\udemy\\io\\abc.txt");
+        File file = new File("C:" + separator + "dev\\repos\\java\\src\\pl\\michalgellert\\udemy\\io\\abc.txt");
         System.out.println(file.exists());
 
         try {
             List<String> lines = Files.readAllLines(Paths.get("C:\\dev\\repos\\java\\src\\pl\\michalgellert\\udemy\\io\\abc.txt"), StandardCharsets.ISO_8859_1);
-            for(String str : lines) {
+            for (String str : lines) {
                 System.out.println(str);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter writer = new FileWriter("C:\\dev\\repos\\java\\src\\pl\\michalgellert\\udemy\\io\\efg.txt", true);
+            writer.write("Hello!");
+            writer.write("\r\n");
+            writer.write("How are you?");
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
