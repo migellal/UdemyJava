38cc960ceff12017613b1d85eac2f1aba7d85a86
migellal <michalgellert@gmail.com>
2018-01-14 21:58:59 +0100

    solution collections

---
+++ src/pl/michalgellert/udemy/solutions/Collections.java
@@ -0,0 +1,14 @@
+package pl.michalgellert.udemy.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(1, 4));
+        points.add(new Point(2, 4));
+        points.add(new Point(3, 7));
+    }
+}
