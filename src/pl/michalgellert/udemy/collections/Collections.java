18cdd26f9fcdeb4cb5b2b15ee4a4c6acd5a2d224
migellal <michalgellert@gmail.com>
2018-01-14 21:02:53 +0100

    set

--- src/pl/michalgellert/udemy/collections/Collections.java
+++ src/pl/michalgellert/udemy/collections/Collections.java
@@ -1,14 +1,23 @@
 package pl.michalgellert.udemy.collections;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Collections {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
+        list.add("axc");
         list.add("tekst");
         list.add("tekst");
+        list.add("abc");
         System.out.println(list);
+
+        Set<String> hashSet = new HashSet<>();
+        Set<String> treeSet = new TreeSet<>();
+
+        hashSet.addAll(list);
+        treeSet.addAll(list);
+        System.out.println(hashSet);
+        System.out.println(treeSet);
     }
 }
